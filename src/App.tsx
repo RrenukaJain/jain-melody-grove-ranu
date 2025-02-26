89984d0a6aad4ce46d4f0121ef43c540adcc8107
RrenukaJain <mjain2347@gmail.com>
2025-02-26 07:32:48 +0530

    Add React Query integration with QueryClientProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '@/lib/react-query';
 import Index from "@/pages/Index";
 import Collection from "@/pages/Collection";
 import NotFound from "@/pages/NotFound";
@@ -8,14 +9,16 @@ import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/collection" element={<Collection />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <Toaster />
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/collection" element={<Collection />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Toaster />
+      </Router>
+    </QueryClientProvider>
   );
 }
 
